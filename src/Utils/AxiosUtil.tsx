d4164f8116322cb0f8fc222ca72bb889f6f9fac2
woqls22 <woqls226@gmail.com>
2022-08-08 21:21:56 +0900

    [Feature] 쿠키로 받은 Request-id 헤더에 포함하여 전송하도록 Axios util수정

--- src/Utils/AxiosUtil.tsx
+++ src/Utils/AxiosUtil.tsx
@@ -4,6 +4,7 @@ import { OAUTH_SERVER } from "./Constants";
 export const headers = {
   "Content-Type": "application/json",
   "Access-Control-Allow-Origin": "*",
+  "request-id": getCookie("request-id"),
 };
 
 export const SpringAxios = axios.create({
@@ -12,3 +13,18 @@ export const SpringAxios = axios.create({
 SpringAxios.interceptors.request.use(function (config) {
   return { ...config, headers: headers };
 });
+
+// 쿠키 가져오기 함수
+function getCookie(cName: string) {
+  cName = cName + "=";
+  var cookieData = document.cookie;
+  var start = cookieData.indexOf(cName);
+  var cValue = "";
+  if (start != -1) {
+    start += cName.length;
+    var end = cookieData.indexOf(";", start);
+    if (end == -1) end = cookieData.length;
+    cValue = cookieData.substring(start, end);
+  }
+  return unescape(cValue);
+}
